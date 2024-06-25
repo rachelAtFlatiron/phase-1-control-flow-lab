a0ce25eb19815881da75512d9d1216d046c08a4e
Rachel Katz <rachel.katz@flatironschool.com>
2024-06-25 12:29:03 -0400

    commit tree fe563fe3d96a5315fe2fc4bb970da48f19834bec

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  
+  //i would use an if/else if because we only want one of these results, and an if else/if structure implies that we only expect one of these test cases to work
+  //2500 feet
+  if(distance > 2500){
+    return('No can do.')
+  }
+  //over 2000 feet
+  else if (distance < 400){
+    return ("This one is on me!")
+  }
+  //between 400 and 2000 feet
+  else if (distance <= 2000 && distance >= 400){
+    return ('That will be twenty bucks.')
+  }
+  //less than 400 feet
+  else if (distance > 2000 && distance < 2500){
+    return('I will gladly take your thirty bucks.')
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+  
+  // if(location === 'NYC'){
+  //   return "Ok, sounds good."
+  // } else {
+  //   return "No go."
+  // }
+
+  // condition ? true result : false result
+  // ternarys evaluate to a single value similarly to 2+2, such that we are going to return the entire ternary and not the individual strings
+  return location === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(value){
+  // if(value === 'generous'){
+  //   return 'Thank you so much.'
+  // } else if (value === 'not as generous') {
+  //   return 'Thank you.'
+  // } else {
+  //   return 'Bye.'
+  // }
+
+  switch(value){
+    case 'generous':
+      //return exits the function so we will never reach the break
+      return 'Thank you so much.'
+      break 
+    case 'not as generous':
+      return 'Thank you.'
+      break 
+    default:
+      return 'Bye.'
+  }
 }
